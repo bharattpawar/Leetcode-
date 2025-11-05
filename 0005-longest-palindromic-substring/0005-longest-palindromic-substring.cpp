8d29b07a082abdeb7ca67f7548f58fc6f47b8535
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2025-11-05 21:11:42 +0530

    Time: 162 ms (25.23%), Space: 13.9 MB (49.51%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    int t[1001][1001];
-
-    int ispalandrome(int start, int end, string &s) {
-        if (start >= end) return 1;  // Single char or empty is palindrome
-        if (t[start][end] != -1) return t[start][end];
-
-        if (s[start] == s[end]) {
-            return t[start][end] = ispalandrome(start + 1, end - 1, s);
-        }
-        return t[start][end] = 0;
-    }
-
+int t[1001][1001];
+int ispalandrome(int start,int end,string &s){
+ if(start>=end)return 1;
+    if(t[start][end]!=-1){
+        return t[start][end];
+  }
+  if(s[start]==s[end]){return t[start][end]=ispalandrome(start+1,end-1,s);}
+   
+  return t[start][end]=0;
+}
     string longestPalindrome(string s) {
-        int n = s.size();
-        memset(t, -1, sizeof(t));
-
-        int max_len = 1;
-        int start_idx = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (ispalandrome(i, j, s) == 1) {
-                    if (j - i + 1 > max_len) {
-                        max_len = j - i + 1;
-                        start_idx = i;
-                    }
-                }
-            }
+      int n=s.size(); int max_ans=0;
+      memset(t,-1,sizeof(t));
+      int ans_start=0; 
+      for(int i=0;i<n;i++){
+      for(int j=i; j<n;j++){
+       if(ispalandrome(i,j,s)==1){
+        if(j-i+1>max_ans){
+            max_ans=j-i+1;
+            ans_start=i;
         }
-
-        return s.substr(start_idx, max_len);
-    }
+       }
+      }}
+ 
+ return s.substr(ans_start,max_ans);
+      }
 };
