23ebe10fac942b62e2b0b39e52111e3324df4073
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-01-14 17:56:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+      int n = arr.size();
+        int start = 0, end = n - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+         return start + k;
+};
