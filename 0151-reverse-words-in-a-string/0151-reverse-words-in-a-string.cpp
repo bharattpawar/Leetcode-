3d2964b9a5df4395a7fca87170196bd6e24cb962
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-03-09 22:24:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -13,8 +13,7 @@ public:
                 s[j++] = s[i++];               // copy word
             }
 
-            while(i < n && s[i] == ' ') i++;   // skip spaces
-
+          
             if(i < n) s[j++] = ' ';            // add single space
         }
 
