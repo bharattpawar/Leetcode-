1435903b3f9229ad09628a4766c17bd5c281143f
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-03-12 20:52:40 +0530

    Time: 14 ms (53.93%), Space: 22.1 MB (34.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-int t[1001][1001];
-int ispalandrome(int start,int end,string &s){
- if(start>=end)return 1;
-    if(t[start][end]!=-1){
-        return t[start][end];
-  }
-  if(s[start]==s[end]){return t[start][end]=ispalandrome(start+1,end-1,s);}
-   
-  return t[start][end]=0;
-}
     string longestPalindrome(string s) {
-      int n=s.size(); int max_ans=0;
-      memset(t,-1,sizeof(t));
-      int ans_start=0; 
-      for(int i=0;i<n;i++){
-      for(int j=i; j<n;j++){
-       if(ispalandrome(i,j,s)==1){
-        if(j-i+1>max_ans){
-            max_ans=j-i+1;
-            ans_start=i;
+        int n = s.size();
+        string ans="";
+
+        for(int i=0;i<n;i++){
+
+            // odd length palindrome
+            int start=i;
+            int end=i;
+
+            while(start>=0 && end<n && s[start]==s[end]){
+                if(end-start+1 > ans.size()){
+                    ans = s.substr(start,end-start+1);
+                }
+                start--;
+                end++;
+            }
+
+            // even length palindrome
+            start=i;
+            end=i+1;
+
+            while(start>=0 && end<n && s[start]==s[end]){
+                if(end-start+1 > ans.size()){
+                    ans = s.substr(start,end-start+1);
+                }
+                start--;
+                end++;
+            }
         }
-       }
-      }}
- 
- return s.substr(ans_start,max_ans);
-      }
+
+        return ans;
+    }
 };
