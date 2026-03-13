0b8afec7106d821b4807fa3c65fdcd6604eaaecf
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-03-13 12:36:41 +0530

    Time: 32 ms (98.64%), Space: 11.1 MB (74.7%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-int isbeautifull(int i,int j,string &s){
-    vector<int>check(26,0);
-    for(int start=i;start<=j;start++){
-        check[s[start]-'a']++;
-    }
-    int large_count=0;
-    int small_count=INT_MAX;
-    for(int start=0;start<=25;start++){
-        if(check[start]>large_count)large_count=check[start];
-        if(check[start]<small_count&&check[start]>0)small_count=check[start];
-    }
-if(large_count!=0&&small_count!=INT_MAX){
-    if(large_count-small_count>0)return large_count-small_count;
-}
-return 0;
-}
     int beautySum(string s) {
-        int ans=0;
-        int n=s.size();
-        for(int i=0; i<n ;i++){
-            for(int j=i ;j<n;j++){
-                ans+=isbeautifull(i,j,s);
-            }
-        }
-        return ans;
+         int n=s.size(); 
+        int i=0; int ans=0;
+        while(i<n){
+                    vector<int>check(26,0);
+ 
+            int j=i; 
+while(j<n){
+    check[s[j]-'a']++;
+       int max=INT_MIN;
+                int min=INT_MAX;
+
+                for(int k=0;k<26;k++){
+                    if(check[k]>0){
+                        if(check[k]>max) max=check[k];
+                        if(check[k]<min) min=check[k];
+                    }
+                }
+                    j++;
+ 
+    ans+=max-min;
+}
+ i++;
+        }return ans;
     }
 };
