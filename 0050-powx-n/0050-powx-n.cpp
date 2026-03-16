3f45afe4eaf9220008a8dbdc0e36bde3c2ed0435
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-03-16 09:53:53 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.2%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    double solve(double x, long n){
-        if(n==0) return 1;
-
-        if(n%2==0){
-            return solve(x*x, n/2);
-        }
-        else{
-            return x * solve(x, n-1);
-        }
+double find(double x,long  n){
+    if(n==0){
+        return 1;
     }
+    if(n%2==0){
+       return find(x*x,n/2);
+    }
+    else{
+       return x*find(x,n-1);
 
+    }
+}
     double myPow(double x, int n) {
-
-        long N = n;
-
-        if(N < 0){
-            x = 1/x;
-            N = -N;
+        long  N=n;
+      
+        if(N<0){
+            x=1/x;
+            N=-N;
         }
-
-        return solve(x,N);
+       return find(x,N);
     }
 };
