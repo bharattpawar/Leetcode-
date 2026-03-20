e656f235332dc29deb317da9aae84e7c249f2a0a
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-03-20 12:45:14 +0530

    Time: 3 ms (27.56%), Space: 11.9 MB (11.87%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-void find( vector<vector<int>>&ans, vector<int>check,vector<int>nums,int i,vector<int>mark){
-if(check.size()==nums.size()){
-    ans.push_back(check);return;
-}
-for(int j=0;j<nums.size();j++){
-    if(mark[j]!=1){
-    check.push_back(nums[j]);
-    mark[j]=1;
-       find(ans,check,nums,i+1,mark);
-       check.pop_back();
-       mark[j]=0;
+void find (vector<vector<int>>&ans,vector<int>nums,int i){
+if(i==nums.size()){
+    ans.push_back(nums);return;
+} 
+for(int j=i;j<nums.size();j++){
+    swap(nums[j],nums[i]);
+       find(ans,nums,i+1);
+    swap(nums[j],nums[i]);
+     
     }
 
 }
  
  
-}
  
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>check;
-vector<int> mark(nums.size(), 0);        find(ans,check,nums,0,mark);
+     
+     find(ans,nums,0);
 return ans;
     }
 };
