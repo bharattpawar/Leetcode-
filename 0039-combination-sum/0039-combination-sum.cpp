15a68651138d71daabf05bd576788bdd7294a8c2
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-03-21 13:15:05 +0530

    Time: 94 ms (5.98%), Space: 72.2 MB (6.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+void find(vector<int>& candidates,vector<vector<int>>&ans,vector<int>check,int i,int sum,int target){
+if(i==candidates.size()||sum>target){
+    return;
+}
+if(sum==target){
+    ans.push_back(check);return;
+}
+check.push_back(candidates[i]);
+find(candidates,ans,check,i,sum+candidates[i],target);
+check.pop_back();
+// sum=sum-candidates[i];
+find(candidates,ans,check,i+1,sum,target);
 
 
-void find(vector<int>arr,vector<vector<int>>&ans,vector<int>check,int n,int i,int sum, int target){
-     if(sum==target){
-            ans.push_back(check); return;
-        }
-    if(i==n||sum>target){
-        
-        return;
-    }check.push_back(arr[i]);
-        find(arr,ans,check,n,i,sum+arr[i],target);
-         check.pop_back();
-     find(arr,ans,check,n,i+1,sum,target);
+
+
+
 
 }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
         vector<int>check;
-        int n=candidates.size();
-        find(candidates,ans,check,n,0,0,target);return ans;
+           int i=0;
+           find(candidates,ans,check,i,0,target);
+           return ans;
     }
 };
