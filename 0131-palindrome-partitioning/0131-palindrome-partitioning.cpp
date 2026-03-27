bd86ade41f4d1e705a03e46605764a4a6b1f95b3
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-03-28 01:03:56 +0530

    Time: 31 ms (85.89%), Space: 53.5 MB (82.52%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int n;
-    bool isPalindrome(string &s,int l,int r){
-        while(l<r){
-            if(s[l] != s[r]) return false;
-            l++;
-            r--;
-        }
-        return true;
+
+
+void find(string &s,vector<vector<string>>&ans,vector<string> &check,int i){
+    if(i==s.size()){
+        ans.push_back(check);
+        return;
     }
-    void backtracking(string s,int idx,vector<string> curr,vector<vector<string>> &result){
-        if(idx == n){
-            result.push_back(curr);
-            return;
-        }
-        for(int i=idx;i<n;i++){
-            if(isPalindrome(s,idx,i)){
-                curr.push_back(s.substr(idx,i-idx+1));
-                backtracking(s,i+1,curr,result);
-                curr.pop_back();
-            }
+    for(int index=i;index<s.size();++index){
+        if(ispaland(s,i,index)){
+            check.push_back(s.substr(i,index-i+1));
+            find(s,ans,check,index+1);
+            check.pop_back();
         }
     }
+}
+bool ispaland(string &s,int i,int index){
+    while(i<=index){
+        if(s[i]!=s[index]){
+            return false;
+        }
+        i++;
+        index--;
+    }return true;
+}
     vector<vector<string>> partition(string s) {
-        n = s.size();
-        vector<vector<string>> result;
-        vector<string> curr;
-        backtracking(s,0,curr,result);
-        return result;
+        vector<vector<string>>ans;
+        vector<string>check;
+        find(s,ans,check,0);return ans;
     }
 };
