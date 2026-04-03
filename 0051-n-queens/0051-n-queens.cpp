94f8a12d1ed9b359c3c8981f20c92d3b70f2cce5
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-04-03 16:10:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -21,16 +21,19 @@ bool dig(vector<string>&board,int i,int j,int n){
     }
     return true;
 }
-void find(vector<vector<string>>& ans,vector<string>&board,vector<int>&column,int i,int n){
+void find(vector<vector<string>>& ans,vector<string>&board,vector<int>&column,int i,int n, vector<int>&rightdigonal,vector<int>&leftdigonal){
 if(i==n){
     ans.push_back(board);return;
 }
 for(int j=0;j<n;j++){
-if(column[j]==0&&dig(board,i,j,n)){
+if(column[j]==0&&rightdigonal[i+j]==0&&leftdigonal[i - j + (n - 1)] == 0){
 column[j]=1;
 board[i][j]='Q';
-find(ans,board,column,i+1,n);
-column[j]=0;
+rightdigonal[i+j]=1;
+leftdigonal[i - j + (n - 1)] = 1;
+find(ans,board,column,i+1,n,rightdigonal,leftdigonal);
+column[j]=0;rightdigonal[i+j]=0;
+leftdigonal[i - j + (n - 1)] = 0;
 board[i][j]='.';
 }
 }
@@ -39,13 +42,16 @@ board[i][j]='.';
 vector<vector<string>>ans;
 vector<string>board(n);
 vector<int>column(n,0);
+vector<int> rightdigonal(2*n - 1, 0);
+vector<int> leftdigonal(2*n - 1, 0);
+
       for(int i=0;i<n;i++){
         for(int j=0;j<n;j++)
         {
              board[i].push_back('.');
         }
       }
-      find(ans,board,column,0,n);
+      find(ans,board,column,0,n,rightdigonal,leftdigonal);
       return ans;
     }
 };
