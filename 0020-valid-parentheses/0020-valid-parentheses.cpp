95bc82ce1bfe005b5bdfeff6c54ac95327fbecb2
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-04-19 17:21:55 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.03%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='['||s[i]=='{'){
+                st.push(s[i]);
+            }
+            else if(!st.empty()&&st.top()=='('){
+                if(s[i]==')'){
+                    st.pop();
+                }
+                else return false;
+            }
+             else if(!st.empty()&&st.top()=='['){
+                if(s[i]==']'){
+                    st.pop();
+                }
+                else return false;
+            }  else if(!st.empty()&&st.top()=='{'){
+                if(s[i]=='}'){
+                    st.pop();
+                }
+                else return false;}else return false;}
+   if(!st.empty())return false;
+         return true;
+    }
+};
