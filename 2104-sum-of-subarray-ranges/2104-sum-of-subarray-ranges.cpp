fe15b7e1aa829bd734c7bb76ba01d2999d3f39d2
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-05-04 06:44:28 +0530

    Time: 7 ms (42.48%), Space: 15.8 MB (40.73%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -0,0 +1,111 @@
+class Solution {
+public:
+void leftans(vector<int>&arr,vector<int>&left,int n){
+    stack<int>st;
+    for(int i=0;i<n;i++){
+       if(st.empty()){
+            left[i]=-1;
+        }
+         else{  while(!st.empty()&&arr[st.top()]>arr[i]){
+            st.pop();
+        } 
+        
+        if(!st.empty())left[i]=st.top();
+
+
+        else left[i]=-1;
+        }
+       
+            
+        st.push(i);
+    }return;
+}
+void rightans(vector<int>&arr,vector<int>&right,int n){
+    stack<int>st;
+    for(int i=n-1;i>=0;i--){
+       if(st.empty()){
+            right[i]=n;
+        }
+         else{  while(!st.empty()&&arr[st.top()]>=arr[i]){
+            st.pop();
+        } 
+        if(!st.empty())right[i]=st.top();
+        else right[i]=n;
+        }
+       
+            
+        st.push(i);
+    }return;
+}
+
+
+
+void leftansm(vector<int>&arr,vector<int>&left,int n){
+    stack<int>st;
+    for(int i=0;i<n;i++){
+       if(st.empty()){
+            left[i]=-1;
+        }
+         else{  while(!st.empty()&&arr[st.top()]<arr[i]){
+            st.pop();
+        } 
+        
+        if(!st.empty())left[i]=st.top();
+
+
+        else left[i]=-1;
+        }
+       
+            
+        st.push(i);
+    }return;
+}
+void rightansm(vector<int>&arr,vector<int>&right,int n){
+    stack<int>st;
+    for(int i=n-1;i>=0;i--){
+       if(st.empty()){
+            right[i]=n;
+        }
+         else{  while(!st.empty()&&arr[st.top()]<=arr[i]){
+            st.pop();
+        } 
+        if(!st.empty())right[i]=st.top();
+        else right[i]=n;
+        }
+       
+            
+        st.push(i);
+    }return;
+}
+    long long subArrayRanges(vector<int>& arr) {
+              int n=arr.size();
+        vector<int>left(n,0);
+        vector<int>right(n,0);
+        leftans(arr,left,n);
+        rightans(arr,right,n);
+        long long sum=0;
+        int M=1e9+7;
+        for(int i=0;i<n;i++){
+            long long la=i-left[i];
+            long long ra=right[i]-i;
+            long long total=la*ra;
+            long long totalsum=arr[i]*total;
+            sum=(sum+totalsum);
+        }
+                  
+        vector<int>leftm(n,0);
+        vector<int>rightm(n,0);
+        leftansm(arr,leftm,n);
+        rightansm(arr,rightm,n);
+         long long sum2=0;
+   
+        for(int i=0;i<n;i++){
+            long long la=i-leftm[i];
+            long long ra=rightm[i]-i;
+            long long total=la*ra;
+            long long totalsum=arr[i]*total;
+            sum2=(sum2+totalsum);
+        }
+        return sum2-sum;
+    }
+};
