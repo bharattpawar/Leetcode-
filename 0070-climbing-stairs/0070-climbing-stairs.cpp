6567cdf567d4e8631dc0bd96fa6811da3d9d90db
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-07-11 13:25:55 +0530

    Time: 0 ms (100%), Space: 8 MB (57.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,14 +8,18 @@ int climb(int n,vector<int>&ans){
     return ans[n]=climb(n-1,ans)+climb(n-2,ans);
 }
     int climbStairs(int n) {
-vector<int>ans(n+1,-1);
+vector<int>ans(3,-1);
 if(n<3)return n;
 ans[0]=0;
 ans[1]=1;
-ans[2]=2; 
-for(int i=3;i<=n;i++){
-ans[i]=ans[i-1]+ans[i-2];
-}return ans[n];
+ans[2]=2;
+long long result=0;int i=3;
+while(i<=n){
+result=ans[2]+ans[1];
+ans[1]=ans[2];
+ans[2]=result;i++;
+}
+return result;
        
     }
 };
