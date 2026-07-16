4c74d259b4cc46fa0ada64fa10df947aed299fdc
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-07-16 14:40:48 +0530

    Time: 0 ms (100%), Space: 10.1 MB (78.92%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -8,13 +8,16 @@ int ans(vector<int>&nums,int n,vector<int>& check){
 }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>check(n,-1);
+        vector<int>check(3,0);
         if(n==1)return nums[0];
         check[0]=nums[0];
         check[1]=max(nums[0],nums[1]);
+        if(n==2)return max(nums[0],nums[1]);
   for(int i=2;i<n;i++){
-    check[i]=max(nums[i]+check[i-2],check[i-1]);
+    check[2]=max(nums[i]+check[0],check[1]);
+    check[0]=check[1];
+    check[1]=check[2];
   }
-        return check[n-1];
+        return check[2];
     }
 };
