42bda1a3d64f1fc4e553ab2d12e9974c7b1cd7ca
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-08-24 12:20:47 +0530

    Time: 0 ms (100%), Space: 23.8 MB (47.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int count1=0;
-        int count2=0;
-        int element1=0;
-        int element2=0;
-        for(int i=0;i<nums.size();i++){
-            if(element2!=nums[i]&&count1==0||element1==nums[i]){ 
-                    element1=nums[i];
-                    count1++;
-              }
-                else if(count2==0||element2==nums[i]){ 
-                    element2=nums[i];
-                    count2++;
-              }
-              else{
+        int ele1 = 0, ele2 = 0;
+        int count1 = 0, count2 = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] == ele1) {
+                count1++;
+            }
+            else if (nums[i] == ele2) {
+                count2++;
+            }
+            else if (count1 == 0) {
+                ele1 = nums[i];
+                count1 = 1;
+            }
+            else if (count2 == 0) {
+                ele2 = nums[i];
+                count2 = 1;
+            }
+            else {
                 count1--;
-count2--;
-              }
-        }
-        int check=nums.size()/3;
-        int check1=0;
-        int check2=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==element1){
-                check1++;
+                count2--;
             }
-            else if(nums[i]==element2){
-                check2++;
-            }
-        }vector<int>ans;
-        if(check2>check){
-            ans.push_back(element2);
         }
-        if(check1>check){
-            ans.push_back(element1);
-        }return ans;
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == ele1)
+                count1++;
+            else if (num == ele2)
+                count2++;
+        }
+
+        vector<int> ans;
+
+        if (count1 > n / 3)
+            ans.push_back(ele1);
+
+        if (count2 > n / 3)
+            ans.push_back(ele2);
+
+        return ans;
     }
 };
