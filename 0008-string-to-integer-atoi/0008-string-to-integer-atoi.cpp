823fe6099efe2b577c0167a2569fd14064f48fe7
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-09-03 21:59:07 +0530

    Time: 1 ms (21.56%), Space: 9.1 MB (81.5%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,35 +1,56 @@
 class Solution {
 public:
+
+    void check(string &s, int i, long long &num) {
+        
+        if (i >= s.size() || s[i] < '0' || s[i] > '9') {
+            return;
+        }
+
+        int digit = s[i] - '0';
+ if (num > INT_MAX)
+            return  ;
+
+        if (num < INT_MIN)
+            return  ;
+        num = num * 10 + digit;
+
+        check(s, i + 1, num);
+    }
+
     int myAtoi(string s) {
-        int n=s.size();long long ans=0;
-        int i=0;int isneg=0;
-        while(i<n&&s[i]==' '){
+        int i = 0;
+        int sign = 1;
+        long long num = 0;
+
+  
+        while (i < s.size() && s[i] == ' ') {
             i++;
         }
-        if(s[i]=='-'){
-            isneg=1;
+
+        if (i >= s.size()) return 0;
+
+       
+        if (s[i] == '-') {
+            sign = -1;
             i++;
         }
-          else if(s[i]=='+'){
-             
+        else if (s[i] == '+') {
             i++;
         }
-        while(i<n){
-            if(s[i]>='0'&&s[i]<='9'){
-                ans=ans*10+(s[i]-'0');
-                if(isneg==1){
-                  if(ans>INT_MAX){
-                    return INT_MIN;
-                  }  
-                }
-                else if(ans>INT_MAX){
-                    return INT_MAX;
-                }
-                i++;
-            }
-            else break;
-        }
-if(isneg==1)return ans*-1;
-return ans;
+
+   
+        check(s, i, num);
+
+        num *= sign;
+
+      
+        if (num > INT_MAX)
+            return INT_MAX;
+
+        if (num < INT_MIN)
+            return INT_MIN;
+
+        return (int)num;
     }
 };
