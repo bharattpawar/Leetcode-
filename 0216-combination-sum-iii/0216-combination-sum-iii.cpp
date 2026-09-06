e44a96fa226702310d28dc986a01bcbee4593cfa
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-09-06 22:17:07 +0530

    Time: 0 ms (100%), Space: 9.1 MB (22.5%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-void find(vector<vector<int>>&ans, vector<int>&check,int k,int n,int sum,int i){
-if(i>10||check.size()>k||sum>n){
-    return;
-}
-if(sum==n&&check.size()==k){
-    ans.push_back(check);
-    return;
-}
-
-check.push_back(i);
-find(ans,check,k,n,sum+i,i+1);
-check.pop_back();
-find(ans,check,k,n,sum,i+1);
-
+void find(vector<vector<int>>&ans,vector<int>&check,int k,int n,int i,int sum){
+        if(sum==n){
+        if(check.size()==k)
+        ans.push_back(check);return;
+    }
+    if(i>9||sum>n||check.size()>k){
+        return;
+    }
+ 
+     
+    check.push_back(i);
+    find(ans,check,k,n,i+1,sum+i);
+ check.pop_back();
+        find(ans,check,k,n,i+1,sum);
 
 
 }
     vector<vector<int>> combinationSum3(int k, int n) {
-      vector<vector<int>>ans;
-      vector<int>check;
-      find(ans,check,k,n,0,1);
-      return ans;
-
+        vector<vector<int>>ans;
+        vector<int>check;int i=1;
+        find(ans,check,k,n,i,0);
+        return ans;
     }
 };
