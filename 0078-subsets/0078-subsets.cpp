0afe8ec75c3331fc0e5100b07a788d6d06702f48
Bharat pawar <143924824+bharattpawar@users.noreply.github.com>
2026-09-06 21:23:14 +0530

    Time: 0 ms (100%), Space: 10 MB (59.5%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-
-
-void find(vector<vector<int>>&ans,vector<int>check,int i,vector<int> nums){
-if(i == nums.size()) {
+void find(vector<int>&nums,vector<vector<int>>&ans,vector<int>&check,int i){
+if(i==nums.size()){
     ans.push_back(check);
     return;
 }
-    check.push_back(nums[i]);
-     find(ans,check,i+1,nums);
-     check.pop_back();
-          find(ans,check,i+1,nums);
-
+check.push_back(nums[i]);
+find(nums,ans,check,i+1);
+check.pop_back();
+find(nums,ans,check,i+1);
 }
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        vector<vector<int>>ans;
-        vector<int>check;
-        
-        find(ans,check,0,nums);
-return ans;
+       int i=0;
+       vector<vector<int>>ans;
+       vector<int>check;
+  
+       find(nums,ans,check,i);
+       return ans; 
     }
 };
